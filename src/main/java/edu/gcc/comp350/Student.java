a879d361403c3bad10e305b4131abe3133adfddc
HANCOCKOR21 <hancockor21@gcc.edu>
2026-02-06 14:36:27 -0500

    Initial commit

---
+++ src/main/java/edu/gcc/comp350/Student.java
@@ -0,0 +1,18 @@
+package edu.gcc.comp350;
+import java.util.ArrayList;
+
+public class Student {
+    int studentID;
+    ArrayList<String> major;
+    ArrayList<String> minor;
+    int gradYear;
+    ArrayList<String> transcript;
+
+    public void createNewStudent(){}
+    public void addCourse(Integer courseID){}
+    public void changeMajor(String newMajor, String oldMajor){}
+    public void addMajor(String newMajor){}
+    public void changeMinor(String newMinor, String oldMinor){}
+    public void addMinor(String OldMinor){}
+    public void changeGradYear(String newYear){}
+}
