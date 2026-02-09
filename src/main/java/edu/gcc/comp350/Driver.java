c47f1b97211c9cfaa437020ea3d9c1859781be3b
HANCOCKOR21 <hancockor21@gcc.edu>
2026-02-09 14:14:37 -0500

    after branch rule in master

--- src/main/java/edu/gcc/comp350/Driver.java
+++ src/main/java/edu/gcc/comp350/Driver.java
@@ -3,5 +3,6 @@ package edu.gcc.comp350;
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Test Git");
+        System.out.println("changed test 2-9-26");
     }
 }
